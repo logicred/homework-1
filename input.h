83fa688cb0ddebc93ca2f1c06e7a14933bedec74
拂石 <754592470@qq.com>
2018-03-25 10:58:24 +0800

    Update input.h

--- input.h
+++ input.h
@@ -1,7 +1,8 @@
-#include"stdio.h"
 #pragma once
+#include"stdio.h"
 
-FILE *fin = NULL;
+extern FILE *fin;
+extern bool fileflag;//文件是否为空标识，true空，false不空
 
 bool input_onefile_open(void);
-void input_onefile_close(void)
+void input_onefile_close(void);
