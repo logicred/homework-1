5074e6cc6e0f108c0ab4f9c9f0375cf5b45d6ec2
拂石 <754592470@qq.com>
2018-03-25 10:52:11 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,34 @@
+#pragma once
 #include"stdio.h"
 #include"if.h"
 #include"input.h"
 #include"output.h"
+#include"count.h"
+#include"store.h"
 
-int main(void) {
+int main() {
 	char c;
 	input_onefile_open();
-	while (true) {
-		c = fscanf(fin, "%c");
-		
-		if (c == EOF)
-			break;
+	while ((c = fgetc(fin)) != EOF) {
+		if (fileflag == false)
+		{
+			count_line();
+			fileflag = true;
+		}
+		if (if_code(c))
+			count_code();
+		if (if_line(c))
+			count_line();
 	}
 	input_onefile_close();
+
 	output_onefile_open();
+
+	output_print_code(countcode);
+	output_print_line(countline);
+
 	output_onefile_close();
+	printf("The program ends\n");
+	getchar();//让VS别一闪而过
 	return(0);
 }
