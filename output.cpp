26d1a3a6ca3bd069f39a3198ab4b883860cd0d79
拂石 <754592470@qq.com>
2018-03-25 10:55:31 +0800

    Update output.cpp

--- output.cpp
+++ output.cpp
@@ -1,8 +1,11 @@
 #include"output.h"
 
+FILE *fout = NULL;//输出文件指针
+
 bool output_onefile_open(void) {
-	fout = fopen("result.txt", "w");
-	if (fout == NULL)
+	errno_t err;
+	err = fopen_s(&fout, "result.txt", "w");
+	if (err == 1)
 	{
 		printf("can't open the file\n");
 		fclose(fout);
@@ -14,4 +17,14 @@ bool output_onefile_open(void) {
 
 void output_onefile_close(void) {
 	fclose(fout);
+}
+
+void output_print_code(long num) {
+	fprintf(fout, "characters:%ld\n", num);
+	return;
+}
+
+void output_print_line(long num) {
+	fprintf(fout, "lines:%ld\n", num);
+	return;
 }
